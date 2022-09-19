1ffb922e9a901518017286874cf0e3ae58ad1556
Francisco Costa <francisco100eg@gmail.com>
2022-09-19 15:12:53 -0300

    Requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,2 +1,23 @@
-//
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import { About } from '../pages';
+
+describe('Teste o componente About', () => {
+  test('Teste se a página contém as informações sobre a Pokédex;', () => {
+    renderWithRouter(<About />);
+    const title = screen.getByRole('heading', {
+      name: 'About Pokédex',
+      level: 2,
+    });
+    const image = screen.getByRole('img');
+
+    expect(title).toBeInTheDocument();
+    expect(image).toBeInTheDocument();
+    expect(image).toHaveAttribute(
+      'src',
+      'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png'
+      + '/800px-Gen_I_Pok%C3%A9dex.png',
+    );
+  });
+});
