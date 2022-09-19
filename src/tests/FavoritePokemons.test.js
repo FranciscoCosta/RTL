d15285525cf575ef26ae958ed833348985d96332
Francisco Costa <francisco100eg@gmail.com>
2022-09-19 15:25:54 -0300

    Requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import { FavoritePokemons } from '../pages';
+
+describe('Teste o componente FavoritePokemons', () => {
+  test(
+    'Teste se é exibida na tela a mensagem No favorite pokemon found,'
+    + 'caso a pessoa não tenha pokémons',
+    () => {
+      renderWithRouter(<FavoritePokemons />);
+
+      const texto = screen.getByText('No favorite pokemon found');
+      expect(texto).toBeInTheDocument();
+    },
+  );
+});
