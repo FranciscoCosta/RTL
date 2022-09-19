e2526414c86f77a528399da80ce5bcbefb464a56
Francisco Costa <francisco100eg@gmail.com>
2022-09-19 14:53:10 -0300

    Requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,13 +1,11 @@
 import React from 'react';
-import userEvent from '@testing-library/user-event';
-import { MemoryRouter } from 'react-router-dom';
-import { render , screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import renderWithRouter from '../renderWithRouter';
 import App from '../App';
 
 describe('Testes do component App', () => {
   test('Testa se a aplicação contém um conjunto fixo de links de navegação:', () => {
-    const { history } = renderWithRouter(<App />);
+    renderWithRouter(<App />);
     const linkHome = screen.getByRole('link', { name: 'Home' });
     const linkAbout = screen.getByRole('link', { name: 'About' });
     const linkFavorite = screen.getByRole('link', {
